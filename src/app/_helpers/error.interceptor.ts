856d582ccc7cd3cba27d6d0ae351125f177af1b0
unknown <HH427@ccs.local>
2026-05-22 15:36:56 +0800

    Fix reset password interceptor issue

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -1,11 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-    HttpRequest,
-    HttpHandler,
-    HttpEvent,
-    HttpInterceptor
-} from '@angular/common/http';
-
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -13,37 +7,18 @@ import { AccountService } from '@app/_services';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-
     constructor(private accountService: AccountService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(catchError(err => {
+            if ([401, 403].includes(err.status) && this.accountService.accountValue) {
+                // auto logout if 401 or 403 response returned from api
+                this.accountService.logout();
+            }
 
-        return next.handle(request).pipe(
-            catchError(err => {
-
-                // Ignore refresh-token errors
-                if (
-                    request.url.includes('/refresh-token')
-                ) {
-                    return throwError(() => err);
-                }
-
-                // Auto logout only for authenticated users
-                if (
-                    [401, 403].includes(err.status) &&
-                    this.accountService.accountValue
-                ) {
-                    this.accountService.logout();
-                }
-
-                const error =
-                    (err && err.error && err.error.message) ||
-                    err.statusText;
-
-                console.error(err);
-
-                return throwError(() => error);
-            })
-        );
+            const error = (err && err.error && err.error.message) || err.statusText;
+            console.error(err);
+            return throwError(() => error);
+        }))
     }
 }
